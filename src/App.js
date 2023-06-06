1740d9aed010da3d80937955944fc5f820b52f09
Valerii Pozhydaiev <p_vf@i.ua>
2023-06-06 07:20:46 +0300

    correct

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/HomePages";
-import Tweets from "./pages/TweetsPages";
+// import  { Home, Tweets } from './pages/index'
 
-// const Home = lazy(() => import("./pages/HomePages"));
-// const Tweets = lazy(() => import("./pages/TweetsPages"));
+const Home = lazy(() => import("./pages/Home"));
+const Tweets = lazy(() => import("./pages/Tweets"));
 
 function App() {
   return (
-    <>
-      <Suspense fallback={<h1>Loading ...</h1>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route index element={<Home />} />
-          <Route path="tweets" element={<Tweets />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Suspense>
-    </>
+    <Suspense fallback={<h1>Loading ...</h1>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="tweets" element={<Tweets />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
   );
 }
 
